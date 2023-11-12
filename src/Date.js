82b8914d128e71c23e31f74c4071b7b2d9c7ec8f
Hyunji Seok <alleyji126@gmail.com>
2023-11-12 13:12:15 +0900

    feat(Date): implement functions to check date and calculate discounts

--- src/Date.js
+++ src/Date.js
@@ -1,5 +1,7 @@
 import { WEEKDAYS, WEEKENDS, SPECIALS, CHRISTMAS, START_DATE, END_DATE } from './constants/dates.js';
+import { ZERO, SPECIAL_PRICE, START_PRICE, ADD_PRICE, ONE } from './constants/events.js';
 import { ERROR } from './constants/messages.js';
+import { discount } from './variables/discounts.js';
 
 class Date {
   #date;
@@ -23,8 +25,43 @@ class Date {
     return this.#date;
   }
 
-  
+  isChristmas() {
+    if (this.#date <= CHRISTMAS) {
+      discount.christmas = true;
+    }
+  }
 
+  isWeekdays() {
+    if (WEEKDAYS.includes(this.#date)) {
+      discount.weekdays = true;
+    }
+  }
+
+  isWeekends() {
+    if (WEEKENDS.includes(this.#date)) {
+      discount.weekends = true;
+    }
+  }
+
+  isSpecials() {
+    if (SPECIALS.includes(this.#date)) {
+      discount.specials = true;
+    }
+  }
+
+  calculateDiscount() {
+    let dateDiscount = ZERO;
+
+    if (discount.christmas) {
+      dateDiscount = dateDiscount + START_PRICE + ADD_PRICE * (this.#date - ONE);
+    }
+
+    if (discount.specials) {
+      dateDiscount = dateDiscount + SPECIAL_PRICE;
+    }
+
+    return dateDiscount;
+  }
 }
 
 export default Date;
