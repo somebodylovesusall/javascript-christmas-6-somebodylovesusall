6ead335a0eaa974196ed5178b3dd064b5e79d771
Hyunji Seok <alleyji126@gmail.com>
2023-11-12 10:29:34 +0900

    feat(Event): implement functions to calculate and show event results

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,61 @@
-class Event {
-  #totalOrderPrice;
+import { BADGE, SANTA_PRICE, STAR_PRICE, TREE_PRICE } from './constants/badges.js';
+import { EVENT, ZERO, CHAMPAGNE_COUNT, DEFAULT } from './constants/events.js';
+import { benefit } from './variables/benefits.js';
 
-  constructor(totalOrderPrice) {
-    this.#totalOrderPrice = totalOrderPrice;
+class Event {
+  #totalOrder;
+  
+  constructor(totalOrder) {
+    this.#totalOrder = totalOrder;
+  }
+
+  calculateTotalDiscount(dateDiscount, menuDiscount) {
+    return dateDiscount + menuDiscount;
+  }
+
+  showFree() {
+    let free = DEFAULT;
+
+    if (benefit.free !== ZERO) {
+      free = CHAMPAGNE_COUNT;
+    }
+
+    return free;
+  }
+
+  showBenefit() {
+    const customerBenefit = [];
+
+    Object.keys(benefit).forEach(key => {
+      if (benefit[key] !== 0) {
+        customerBenefit.push([EVENT[key], benefit[key]]);
+      }
+    });
+
+    return customerBenefit;
+  }
+
+  calculateTotalBenefit() {
+    return Object.values(benefit).reduce((acc, value) => acc + value, 0);
+  }
+
+  calculateTotalPay() {
+    return this.#totalOrder - this.calculateTotalDiscount();
+  }
+
+  awardBadge() {
+    const totalBenefit = this.calculateTotalBenefit();
+    let badge = DEFAULT;
+
+    if (totalBenefit >= STAR_PRICE && totalBenefit < TREE_PRICE) {
+      badge = BADGE.star;
+    } else if (totalBenefit >= TREE_PRICE && totalBenefit < SANTA_PRICE) {
+      badge = BADGE.tree;
+    } else if (totalBenefit >= SANTA_PRICE) {
+      badge = BADGE.santa;
+    }
+
+    return badge;
   }
 }
 
