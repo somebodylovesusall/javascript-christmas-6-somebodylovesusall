60a83221b63061cd77cb82fdd68534a23c63768e
Hyunji Seok <alleyji126@gmail.com>
2023-11-13 12:01:22 +0900

    feat(InputView): implement functions to receive input data

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,9 +1,35 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { COMMA, INPUT, LINE_BREAK } from '../constants/messages.js';
+import Date from '../Date.js';
+import Order from '../Order.js';
+
 const InputView = {
-  async readDate() {
-    const input = await Console.readLineAsync('12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)');
-    
-  }
+  async enterDate() {
+    try {
+      const input = await MissionUtils.Console.readLineAsync(`${INPUT.date}${LINE_BREAK}`);
+      const date = new Date(input);
+
+      return date;
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+      return this.enterDate();
+    }
+  },
   
+  async enterOrder() {
+    try {
+      const input = await MissionUtils.Console.readLineAsync(`${INPUT.order}${LINE_BREAK}`);
+      const order = new Order(input.split(COMMA));
+
+      return order;
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+      return this.enterOrder();
+    }
+  },
 }
+
+await InputView.enterDate();
+await InputView.enterOrder();
 
 export default InputView;
